60a03eb0ecea8df6f93c567b6a43967717515714
likerhythm <89333351+likerhythm@users.noreply.github.com>
2022-07-12 23:16:09 +0900

    Update app.js

--- nodebird/app.js
+++ nodebird/app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+//session 설정(express-session 패키지)
 app.use(session({
 	resave: false,
 	saveUninitialized: false,
